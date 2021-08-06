8e9c814bc3f64ecdf00161ad83358257fbdd34d8
Roland Müller <2827778+rolandm@users.noreply.github.com>
2021-08-06 21:45:42 +0300

    myapplication; greeting message changes when activity is (re)created

--- myapplication/src/main/java/person/mueller/roland/myapplication/MyActivity.java
+++ myapplication/src/main/java/person/mueller/roland/myapplication/MyActivity.java
@@ -1,7 +1,12 @@
 package person.mueller.roland.myapplication;
 
+import android.util.Log;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.util.Date;
+
 
 public class MyActivity extends AppCompatActivity {
 
@@ -9,5 +14,17 @@ public class MyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+
+        setGreeting();
     }
+
+
+    private void setGreeting()
+    {
+        Log.wtf(this.getClass().getCanonicalName(), "Setting Greeting");
+        TextView greeting = (TextView)findViewById( R.id.greeting );
+        Date tmStamp = new Date();
+        greeting.setText( "Moi! " + tmStamp);
+    }
+
 }
